e700f8479438d2fc2170f8013743ec99441aa884
Leonid Petrov <ld1995@tut.by>
2017-07-10 15:23:40 +0300

    view + sql

--- src/main/resources/static/js/datatable.js
+++ src/main/resources/static/js/datatable.js
@@ -1,15 +1,16 @@
 $(document).ready( function () {
-    var table = $('#employeesTable').DataTable({
+    var table = $('#patientTable').DataTable({
         "sAjaxSource": "/patients",
         "sAjaxDataProp": "",
         "order": [[ 0, "asc" ]],
         "aoColumns": [
             { "mData": "id"},
-            { "mData": "name" },
-            { "mData": "lastName" },
-            { "mData": "email" },
-            { "mData": "phone" },
-            { "mData": "active" }
+            { "mData": "wards" },
+            { "mData": "number" },
+            { "mData": "date" },
+            { "mData": "last" },
+            { "mData": "first" },
+            { "mData": "second" }
         ]
     })
 });
